88edeb7659a38890545607c8340fab385b1ef86d
Fwiffo <borgified@gmail.com>
2013-01-18 17:22:52 -0800

    step2 pretty much complete.. ready for step3

--- ui/step2.pl
+++ ui/step2.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use CGI qw /:standard/;
 use DBI;
-
+use Carp;
 
 my $my_cnf = '/secret/my_cnf.cnf';
 
@@ -13,6 +13,7 @@ my $dbh = DBI->connect("DBI:mysql:" . ";mysql_read_default_file=$my_cnf" .';mysq
 my $sql = "describe pkmud";
 my $sth=$dbh->prepare($sql) or die ("Cannot prepare: ".$dbh->errstr());
 $sth->execute();
+
 
 #my %columns;
 my $i=0;
@@ -27,27 +28,59 @@ my $cgi = new CGI;
 
 my @params=$cgi->param('categories');
 
-%type = (
+my %format = (
 	damage		=> 'text',
 	namelist	=> 'text',
 	spells		=> 'text',
 	whereload	=> 'text',
+	charges		=> 'text',
 	area			=> 'area',
 	type			=> 'type',
 	);
 
+my $string;
+
+my $types=$dbh->prepare("select distinct(type) from pkmud");
+my $areas=$dbh->prepare("select distinct(area) from pkmud");
 
 foreach my $param (@params){
-
-	if($type{$param} eq 'text'){
-	}elsif($type{$param} eq 'type'){
-#select distinct(type) from pkmud
-	}elsif($type{$param} eq 'area'){
-#select distinct(area) from pkmud
-	}else{
-#treat as if numerical
+	if(!exists($format{$param})){
+#treat as if numerical (default)
 		#select max($param) from pkmud
 		#select min($param) from pkmud
+		my $minmax=$dbh->prepare("select min($param),max($param) from pkmud");
+		$minmax->execute();
+		my($min,$max)=$minmax->fetchrow_array;
+		if($min eq ''){
+			$min=0;
+		}
+
+		$string=$string."<tr><td>$param</td><td><select name='$param.logic'><option value='1'>=</option><option value='2'>&lt;</option><option value='3'>&gt;</option></td><td><input type='text' name='$param'></td><td>range: ($min,$max)</td></tr>";
+
+	}elsif($format{$param} eq 'text'){
+		$string=$string."<tr><td>$param</td><td>eq</td><td><input type='text' name='$param'></td><td>string match</td></tr>";
+	}elsif($format{$param} eq 'type'){
+#select distinct(type) from pkmud
+		$types->execute();
+		my @types;
+		while(my $type=$types->fetchrow_array){
+			push(@types,$type);
+		}
+		@types=sort(@types);
+		my $dropdown = $cgi->popup_menu(-name=>$param, -values=>\@types);
+		$string=$string."<tr><td>$param</td><td></td><td>$dropdown</td><td></td></tr>";
+	}elsif($format{$param} eq 'area'){
+#select distinct(area) from pkmud
+		$areas->execute();
+		my @areas;
+		while(my $area=$areas->fetchrow_array){
+			push(@areas,$area);
+		}
+		@areas=sort(@areas);
+		my $dropdown = $cgi->popup_menu(-name=>$param, -values=>\@areas);
+		$string=$string."<tr><td>$param</td><td></td><td>$dropdown</td><td></td></tr>";
+	}else{
+		die "i shouldnt be here";
 	}
 }
 
@@ -61,7 +94,9 @@ determine how you would like to filter your results
 <br>
 $submit
 <br>
-@params
+<table>
+$string
+</table>
 
 </form>
 END
